931b08f3a5250309d12a64c7fd30ad5a88138627
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 00:55:49 +0300

    Scripted auto-commit on change (2020-10-21 00:55:49) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -5,6 +5,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkExample {
+    
     SparkConf conf = new SparkConf().setAppName("sample");
     JavaSparkContext sc = new JavaSparkContext(conf);
     JavaRDD<String> lines = sc.textFile("hdfs://");
