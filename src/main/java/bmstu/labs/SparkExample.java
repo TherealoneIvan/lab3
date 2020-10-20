a01a37f3a72b002bbfb8895daae599710b7cbd05
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 01:10:10 +0300

    Scripted auto-commit on change (2020-10-21 01:10:10) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -1,12 +1,14 @@
 package bmstu.labs;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class SparkExample {
     SparkConf conf = new SparkConf().setAppName("sample");
     JavaSparkContext sc = new JavaSparkContext(conf);
     JavaRDD<String> inputRDD = sc.textFile("hdfs://");
-    
+    JavaPairRDD<Tuple2<String , String> , >
 }
