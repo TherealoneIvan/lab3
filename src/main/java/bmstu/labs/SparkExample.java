883d091cb9ec59c3a349d87699574ff0075f5012
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 00:38:39 +0300

    Scripted auto-commit on change (2020-10-21 00:38:39) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -1,10 +1,11 @@
 package bmstu.labs;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkExample {
     SparkConf conf = new SparkConf().setAppName("sample");
     JavaSparkContext sc = new JavaSparkContext(conf);
-    
+    JavaRDD<String> lines = sc.textFile("/path/to/README.md");
 }
