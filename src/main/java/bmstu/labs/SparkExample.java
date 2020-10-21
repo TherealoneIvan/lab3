2c084976059b2ea130d6fa216b8bee8a6812a647
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 18:45:39 +0300

    Scripted auto-commit on change (2020-10-21 18:45:39) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -6,7 +6,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.function.Function;
 public class SparkExample {
 
     public static final String flightRegex = ",";
