f22362b31ea00948c87896475d13a97adbe2de5e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 18:35:32 +0300

    Scripted auto-commit on change (2020-10-21 18:35:32) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -6,7 +6,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-class P
+class FlightPartitioner()
 public class SparkExample {
 
     public static final String flightRegex = ",";
