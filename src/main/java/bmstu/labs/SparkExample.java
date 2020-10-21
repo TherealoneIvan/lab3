00c9ebc4bc3349c9d5eeb74fc2e939025e2ab1c5
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-22 01:17:51 +0300

    Scripted auto-commit on change (2020-10-22 01:17:51) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -53,9 +53,11 @@ public class SparkExample {
                 );
 
         JavaPairRDD<Tuple2<Integer, Integer>, FlightDataSerializable> reducedRes = resRDD.reduceByKey(
-                (item , FlightDataSerializable)->{
-                    
-                }
+                item -> new FlightDataSerializable(
+                        item.getOriginAiroportID(),
+                        item.getDestAiroportID()
+                ),
+
         );
     }
 }
