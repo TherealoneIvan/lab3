a71f231f4297f2a1e6b6f017d6ef6bfb1f008729
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-21 19:00:01 +0300

    Scripted auto-commit on change (2020-10-21 19:00:01) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -4,9 +4,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import scala.Tuple2;
 
-import java.util.function.Function;
 
 public class SparkExample {
 
