6b59d57779c8ac73230bd2ee377d0a20cc4d6de5
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-22 15:19:28 +0300

    Scripted auto-commit on change (2020-10-22 15:19:28) by gitwatch.sh

--- src/main/java/bmstu/labs/SparkExample.java
+++ src/main/java/bmstu/labs/SparkExample.java
@@ -60,6 +60,6 @@ public class SparkExample {
                                                             p
                         ),
                     FlightDataSerializable::Add
-                )
+                );
     }
 }
