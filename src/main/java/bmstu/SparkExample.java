518de391a2149df6c21942535438605792a012b8
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-25 20:05:56 +0300

    Scripted auto-commit on change (2020-10-25 20:05:56) by gitwatch.sh

--- src/main/java/bmstu/SparkExample.java
+++ src/main/java/bmstu/SparkExample.java
@@ -64,7 +64,7 @@ public class SparkExample {
         JavaPairRDD<Tuple2<Integer, Integer>, AiroportDataSeriazable> resRDD = inputFlightRDD
                 .mapToPair(line -> {
                             String[] items = line.split(flightRegex);
-                            if (items[originCanceldID].equals("1")) {
+                            if (items[].equals("1")) {
                                 return new Tuple2<>(new Tuple2<>(Integer.parseInt(items[originAiroportID])
                                         , Integer.parseInt(items[originDestID])),
                                         new AiroportDataSeriazable(
