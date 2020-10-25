f4f9badaf8dcce7693db87ff9d6d63fbb4ca786d
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-25 20:02:34 +0300

    Scripted auto-commit on change (2020-10-25 20:02:33) by gitwatch.sh

--- src/main/java/bmstu/SparkExample.java
+++ src/main/java/bmstu/SparkExample.java
@@ -72,7 +72,7 @@ public class SparkExample {
                                                 Integer.parseInt(items[originDestID]),
                                                 0,
                                                 true));
-                            } else  {
+                            } else if (items[originCanceldID].equals("")) {
                                 return new Tuple2<>(new Tuple2<>(Integer.parseInt(items[originAiroportID])
                                         , Integer.parseInt(items[originDestID])),
                                         new AiroportDataSeriazable(
