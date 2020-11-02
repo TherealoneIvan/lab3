79954d8bc663186fc4f6788dcc003307743f0cda
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-11-02 19:03:01 +0300

    Code refactoring

--- src/main/java/bmstu/SparkExample.java
+++ src/main/java/bmstu/SparkExample.java
@@ -13,14 +13,62 @@ import java.util.Map;
 
 
 public class SparkExample {
-
-    public static final String flightRegex = ",";
-    public static final int originAiroportID = 11;
-    public static final int originDestID = 14;
-    public static final int originDelayID = 18;
-    public static final int originCanceldID = 19;
+    public static final int ID_COLUM_ID = 1;
+    public static final int AIRPORTS_ID_COLUM_ID = 0;
+    public static final String FLIGHT_DELIMETR = ",";
+    public static final int ORIGIN_AIROPORT_ID = 11;
+    public static final int ORIGIN_DEST_ID = 14;
+    public static final int ORIGIN_DELAY_ID = 18;
+    public static final int ORIGIN_CANCELD_ID = 19;
     public static final String REGEX_BACKSLASH = "\"";
     public static final String EMPTY_STRING = "";
+
+    public static String removeQuates(String line){
+        return line.replace(REGEX_BACKSLASH, EMPTY_STRING);
+    }
+    public static String concanitate(String [] arrayOfStrings , int firstItemNumber){
+        StringBuilder builder = new StringBuilder();
+        for ( int i = firstItemNumber; i < arrayOfStrings.length; i++){
+            builder.append(arrayOfStrings[i]);
+        }
+        return builder.toString();
+    }
+    private static final Function2 REMOVE_HEADER = new Function2<Integer , Iterator<String> , Iterator<String>>() {
+        @Override
+        public Iterator<String> call(Integer ind, Iterator<String> iterator) throws Exception {
+            if (ind == 0 && iterator.hasNext()){
+                iterator.next();
+                return iterator;
+            }else {
+                return iterator;
+            }
+        }
+    };
+    private static Tuple2<Integer,String> makePair(String line) {
+        String[] airportsNames = line.split(FLIGHT_DELIMETR);
+        String airportsConcatinetedID= concanitate(airportsNames , ID_COLUM_ID);
+        String airport = airportsNames[AIRPORTS_ID_COLUM_ID];
+        String preParseID = removeQuates(airportsConcatinetedID);
+        int airaceID = Integer.parseInt(preParseID);
+        return new Tuple2<>(airaceID, airport);
+    }
+
+    private static Tuple2<Tuple2<Integer, Integer>, AiroportDataSeriazable>  makeFlightData (String line){
+        String[] items = line.split(FLIGHT_DELIMETR);
+        double timeDelay = items[ORIGIN_DELAY_ID].isEmpty() ? 0
+                : Double.parseDouble(items[ORIGIN_DELAY_ID]);
+        Boolean isCanceled = items[ORIGIN_CANCELD_ID].isEmpty();
+        return new Tuple2<>(
+            new Tuple2<>(Integer.parseInt(items[ORIGIN_AIROPORT_ID]),
+                         Integer.parseInt(items[ORIGIN_DEST_ID])),
+            new AiroportDataSeriazable(
+                    Integer.parseInt(items[ORIGIN_AIROPORT_ID]),
+                    Integer.parseInt(items[ORIGIN_DEST_ID]),
+                    timeDelay,
+                    isCanceled
+            )
+        );
+    }
 
     public static void main(String args[]) throws Exception {
         if (args.length != 3) {
@@ -31,65 +79,23 @@ public class SparkExample {
         String flightPath = args[1];
         SparkConf conf = new SparkConf().setAppName("sample");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        Function2 removeHeader = new Function2<Integer , Iterator<String> , Iterator<String>>(){
-            @Override
-            public Iterator<String> call(Integer ind, Iterator<String> iterator) throws Exception {
-                if (ind == 0 && iterator.hasNext()){
-                    iterator.next();
-                    return iterator;
-                }else {
-                    return iterator;
-                }
-            }
-        };
+
         JavaRDD<String> inputAiroportRDD = sc.textFile(airoportPath)
-                .mapPartitionsWithIndex(removeHeader , false);
+                .mapPartitionsWithIndex(REMOVE_HEADER, false);
         JavaRDD<String> inputFlightRDD = sc.textFile(flightPath)
-                .mapPartitionsWithIndex(removeHeader , false);
+                .mapPartitionsWithIndex(REMOVE_HEADER, false);
         JavaPairRDD<Integer , String> airoportNames = inputAiroportRDD
-                .mapToPair( line -> {
-                            String[] airportsNames = line.split(flightRegex);
-                            String airport = "";
-                            for (int i = 1; i < airportsNames.length; i++)
-                                airport += airportsNames[i];
-                            String preParseID = airportsNames[0]
-                                    .replace(REGEX_BACKSLASH, "");
-                            int airaceID = Integer.parseInt(preParseID);
-                            return new Tuple2<>(airaceID , airport);
-                        }
-                );
+                .mapToPair(SparkExample::makePair);
 
         Map<Integer , String> airName = airoportNames.collectAsMap();
 
         JavaPairRDD<Tuple2<Integer, Integer>, AiroportDataSeriazable> resRDD = inputFlightRDD
-                .mapToPair(line -> {
-                            String[] items = line.split(flightRegex);
-                            if (items[originDelayID].equals("")) {
-                                return new Tuple2<>(new Tuple2<>(Integer.parseInt(items[originAiroportID])
-                                        , Integer.parseInt(items[originDestID])),
-                                        new AiroportDataSeriazable(
-                                                Integer.parseInt(items[originAiroportID]),
-                                                Integer.parseInt(items[originDestID]),
-                                                0,
-                                                true));
-                            } else {
-                                return new Tuple2<>(new Tuple2<>(Integer.parseInt(items[originAiroportID])
-                                        , Integer.parseInt(items[originDestID])),
-                                        new AiroportDataSeriazable(
-                                                Integer.parseInt(items[originAiroportID]),
-                                                Integer.parseInt(items[originDestID]),
-                                                Double.parseDouble(items[originDelayID]),
-                                                false));
-                            }
-                        }
-                );
+                .mapToPair(SparkExample::makeFlightData);
 
         JavaPairRDD<Tuple2<Integer, Integer>, FlightDataSerializable> reducedRes = resRDD
                 .combineByKey(
                     p -> {
-                            int delayedCnt = 0;
-                            if (p.getDestAiroportID() > 0 || p.isCanceld())
-                                delayedCnt++;
+                            int delayedCnt = p.getDestAiroportID() > 0 || p.isCanceld() ? 1 : 0;
                             return new FlightDataSerializable(p.getTimeDelay() , delayedCnt , 1);
                     },
                     FlightDataSerializable::addValue,
@@ -114,4 +120,3 @@ public class SparkExample {
         resOutput.saveAsTextFile("hdfs://localhost:9000/user/macos/output");
     }
 }
-///Users/macos/IdeaProjects/lab3/target
